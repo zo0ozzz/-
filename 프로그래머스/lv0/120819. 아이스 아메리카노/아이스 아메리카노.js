a4047eb685b4baeaf8b8f3650caa0e982575ced6
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-05 00:15:15 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,16 +1,29 @@
 function solution(money) {
-    // 상수 자원에서 최대로. -> 특정 조건을 만족하는 마지막 변수.
+    return [Math.floor(money / 5500), money % 5500]
     
-    if(money < 5500) {
-        return [0, money]
-    }
+//     if(money < 5500) {
+//         return [0, money]
+//     }
     
      
-    for(i = 1; ; i++) {
-        const rest = money - 5500 * i;
-            if(rest >= 0 && rest < 5500) {
-                return [i, rest];
-            }
-            continue
-        }   
+//     for(i = 1; ; i++) {
+//         const rest = money - 5500 * i;
+//             if(rest >= 0 && rest < 5500) {
+//                 return [i, rest];
+//             }
+//             continue
+//         }
+    
+    // return [Math.floor(money / 5500), money % 5500]
+    
+//     let count = 0;
+    
+//     while(money >= 5500) {
+//         money = money - 5500;
+//         count++
+//     }
+    
+//     return [count, money]
+    
+    
 }
