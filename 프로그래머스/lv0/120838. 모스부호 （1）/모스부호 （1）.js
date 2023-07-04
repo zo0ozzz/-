5335ff65ec914c39d38ecb0d866bc2b65aad916a
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-04 20:57:44 +0900

    [level 0] Title: 모스부호 (1), Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,36 @@
+function solution(letter) {
+    var answer = '';
+    
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    const letterArray = letter.split(' ');
+    
+    let morseArray = [];
+    let alphabetArray = [];
+    
+    for(key in morse) {
+        morseArray.push(key)
+        alphabetArray.push(morse[key])
+    }
+    
+    let indexArray = [];
+    letterArray.map((item1, index1) => {
+        morseArray.forEach((item2, index2) => {
+            if(item1 === item2) {
+                indexArray[index1] = index2;
+            }
+        })
+    })
+    
+    const resultArray = indexArray.map(index => {
+        return alphabetArray[index]
+    })
+    
+    return resultArray.join('')
+}
