27f6d7aa803d5873e16e5e2e63bd46576a0acc71
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-10 20:43:09 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,20 +1,24 @@
 function solution(box, n) {
     // ~의 최댓값 최솟값을 구하는 방식.
-    // n * 개수 <= 길이. 여기서 n의 최댓값.
+    // - while(조건)이나 나누기 Math가 자주 활용됨.
     
     const [가로, 세로, 높이] = box;
     
-    const result = box.reduce((acc, item) => {
-        let i = 1;
-        
-        while(n * i <= item) {
-            i = i + 1;
-        }
-        
-        acc = acc * (i - 1);
-        
-        return acc;
-    }, 1)
+    const diceAmount = Math.floor(가로 / n) * Math.floor(세로 / n) * Math.floor(높이 / n)
     
-    return result;
+    return diceAmount;
+    
+//     const result = box.reduce((acc, item) => {
+//         let i = 1;
+        
+//         while(n * i <= item) {
+//             i = i + 1;
+//         }
+        
+//         acc = acc * (i - 1);
+        
+//         return acc;
+//     }, 1)
+    
+//     return result;
 }
