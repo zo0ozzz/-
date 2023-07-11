9837a4f60fc291d591a019bcedcd361e030011ea
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-11 10:23:37 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,10 +1,18 @@
 function solution(my_string) {
-    const regex = /[a-z]/g;
+    const regex = /\d/g;
     
-    const filtered = my_string.replace(regex, '');
-    const toArray = filtered.split('');
-    const toNumber = toArray.map(item => parseInt(item))
-    const sorted = toNumber.sort();
+    const filtered = my_string.match(regex);
     
-    return sorted;
+    const result = filtered.map(item => parseInt(item)).sort();
+    
+    return result;
+    
+//     const regex = /[a-z]/g;
+    
+//     const filtered = my_string.replace(regex, '');
+//     const toArray = filtered.split('');
+//     const toNumber = toArray.map(item => parseInt(item))
+//     const sorted = toNumber.sort();
+    
+//     return sorted;
 }
