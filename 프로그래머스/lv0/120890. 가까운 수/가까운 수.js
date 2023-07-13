499a5ecd844938267dcb1341e987efcb2bd98dbd
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-13 13:19:41 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,23 +1,37 @@
-function solution(array, n) { 
-    function howNear(n, element) {
-        return Math.abs(n - element)
-    }
-    
-    const newArray = array.sort((a, b) => howNear(n, a) - howNear(n, b))
-    
-    if(howNear(n, newArray[0]) === howNear(n, newArray[1])) {
-        let arr = [];
-    
-        for(i = 0; i < newArray.length; i++) {
-            if(howNear(n, newArray[i]) === howNear(n, newArray[0])) {
-                arr.push(newArray[i]);
+function solution(array, n) {
+    return array.reduce((acc, cur) => {
+        if(Math.abs(n - cur) <= Math.abs(n - acc)) {
+            if(Math.abs(n - cur) === Math.abs(n - acc)) {
+                acc = [acc, cur].sort((a, b) => a - b)[0];
+            }
+            else {
+                acc = cur;
             }
         }
+        
+        return acc;
+    }, 1000)
+    
+    
+    
+    
+//     function howNear(n, element) {
+//         return Math.abs(n - element)
+//     }
+    
+//     const newArray = array.sort((a, b) => howNear(n, a) - howNear(n, b))
+    
+//     if(howNear(n, newArray[0]) === howNear(n, newArray[1])) {
+//         let arr = [];
+    
+//         for(i = 0; i < newArray.length; i++) {
+//             if(howNear(n, newArray[i]) === howNear(n, newArray[0])) {
+//                 arr.push(newArray[i]);
+//             }
+//         }
 
-        return Math.min(...arr);    
-    } else {
-        return newArray[0]
-    }
-    
-    
+//         return Math.min(...arr);    
+//     } else {
+//         return newArray[0]
+//     }
 }
