98d9f0aa4a8d6f3a5e4ac80e3bfe83b7ccde6152
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-13 14:27:40 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,4 +1,16 @@
-function solution(my_string) {
-    var answer = '';
-    return answer;
+function solution(my_string) { 
+    let converted = '';
+    
+    for(item of my_string) {
+        if(item === item.toUpperCase()) {
+            converted += item.toLowerCase();   
+        }
+        else {
+            converted += item.toUpperCase();   
+        }
+    }
+    
+    return converted;
+    
+    // return my_string.replace(/./g, item => /[A-Z]/.test(item) ? item.toLowerCase() : item.toUpperCase())
 }
