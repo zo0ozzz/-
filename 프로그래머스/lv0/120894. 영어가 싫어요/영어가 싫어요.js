65450496831d9774cc0e881ac11b890652baa0b4
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-14 14:58:43 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -14,33 +14,9 @@ function solution(numbers) {
 
   const reg = new RegExp(ddd, "g");
 
-  return parseInt(numbers.match(reg).map((item) => convert(item)).join(""));
-}
-
-function convert(item) {
-  switch (item) {
-      case "zero":
-          return 0;function solution(numbers) {
-  const ddd = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ].join("|");
-
-  const reg = new RegExp(ddd, "g");
-
   return parseInt(
     numbers
-      .match(reg)
-      .map((item) => convert(item))
-      .join("")
+      .replace(reg, (item) => convert(item))
   );
 }
 
@@ -48,27 +24,6 @@ function convert(item) {
   switch (item) {
     case "zero":
       return 0;
-    case "one":
-      return 1;
-    case "two":
-      return 2;
-    case "three":
-      return 3;
-    case "four":
-      return 4;
-    case "five":
-      return 5;
-    case "six":
-      return 6;
-    case "seven":
-      return 7;
-    case "eight":
-      return 8;
-    case "nine":
-      return 9;
-  }
-}
-
     case "one":
       return 1;
     case "two":
