6174ae1ac2b41de63f23c51d52621d5e0f578928
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-14 15:15:45 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,14 @@
+function solution(my_string, num1, num2) {
+    const arr = my_string.split('');
+    
+    const item1 = arr[num1];
+    const item2 = arr[num2];
+    
+    const copy = [...arr];
+    
+    copy[num1] = item2;
+    copy[num2] = item1;
+    
+    return copy.join('');
+    
+}
