8698ccf8a819e713e60ca7c6d79905b8df77dd5a
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-14 17:47:16 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,20 +1,40 @@
 function solution(s) {
-    const unique = [...new Set(s.split(''))];
+    let result = [];
     
-    let arr = [];
+    for(item of s) if(s.indexOf(item) === s.lastIndexOf(item)) result.push(item)
     
-    for(item of unique) {
-        let count = 0;
+    return result.sort().join('');
+    
+//     const unique = [...new Set(s.split(''))];
+    
+//     const result = unique.reduce((acc, cur) => {
+//         const reg = new RegExp(cur, 'g');
         
-        for(i = 0; i < s.length; i++) {
-            if(item === s[i]) count++
+//         const count = s.match(reg).length;
+        
+//         if(count === 1) acc.push(cur);
+        
+//         return acc;
+//     }, []);
+    
+//     return result.length !== 0 ? result.sort().join('') : '';
+    
+//     const unique = [...new Set(s.split(''))];
+    
+//     let arr = [];
+    
+//     for(item of unique) {
+//         let count = 0;
+        
+//         for(i = 0; i < s.length; i++) {
+//             if(item === s[i]) count++
             
-            if(count === 2) break;
-        }
-        if(count === 1) arr.push(item)
-    }
+//             if(count === 2) break;
+//         }
+//         if(count === 1) arr.push(item)
+//     }
     
-    if(arr.length === 0) return '';
+//     if(arr.length === 0) return '';
     
-    return arr.sort().join('');    
+//     return arr.sort().join('');    
 }
