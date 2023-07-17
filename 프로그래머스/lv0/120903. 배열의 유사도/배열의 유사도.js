5bcc25b4493379a2221908e09d2ecfe05a5e4ebd
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-17 22:43:36 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,16 +1,18 @@
 function solution(s1, s2) {
-    const arr1 = [...new Set(s1)];
-    const arr2 = [...new Set(s2)];
+//     const arr1 = [...new Set(s1)];
+//     const arr2 = [...new Set(s2)];
     
-    let count = 0;
+//     let count = 0;
     
-    for(item1 of arr1) {
-        for(item2 of arr2) {
-            if(item1 === item2) {
-                count++
-            }
-        }
-    }
+//     for(item1 of arr1) {
+//         for(item2 of arr2) {
+//             if(item1 === item2) {
+//                 count++
+//             }
+//         }
+//     }
     
-    return count;
+//     return count;
+    
+    return s1.filter(item => s2.includes(item)).length;
 }
