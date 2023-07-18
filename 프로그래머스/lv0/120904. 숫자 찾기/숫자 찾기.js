5fa355d5c66f41067e65264127fbbfdb090dcce4
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-18 11:06:08 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,9 +1,13 @@
 function solution(num, k) {
-    const regex = new RegExp(k);
+    const isK = ('' + num).indexOf(k);
     
-    const match = num.toString().match(regex);
+    return isK === -1 ? -1 : isK + 1;
     
-    return match ? match.index + 1 : -1;
+//     const regex = new RegExp(k);
+    
+//     const match = num.toString().match(regex);
+    
+//     return match ? match.index + 1 : -1;
 
 //     if(num.toString().split('').indexOf(k.toString()) === -1) {
 //         return -1;
