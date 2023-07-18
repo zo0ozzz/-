43d78dd925e6932b5777e7248b7155319c645044
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-18 14:17:43 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.32 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/README.md
+++ 프로그래머스/lv0/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.26 ms
+메모리: 37.1 MB, 시간: 1.32 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,36 +1,58 @@
 function solution(array) {
-    if(array.length === 1) {
-        return array[0];
-    }
-    
-  const unique = [...new Set(array)];
-    if(unique.length === 1) {
-        return unique[0];
-    }
-
-  const newArray = unique.map((item) => {
+      function count(element) {
     let count = 0;
 
-    for (i = 0; i < array.length; i++) {
-      if (item === array[i]) {
+    for (item of array) {
+      if (item === element) {
         count++;
       }
     }
 
-    return [item, count];
-  });
+    return count;
+  }
 
-  const copy = [...newArray];
+  const unique = [...new Set(array)];
 
-  copy.sort((first, second) => {
+  const sorted = unique.sort((a, b) => count(b) - count(a));
 
-    return second[1] - first[1];
-  });
+  if (count(sorted[0]) === count(sorted[1])) {
+    return -1;
+  }
+
+  return sorted[0];
     
-    if(copy[0][1] === copy[1][1]) {
-        return -1;
-    }
-    else {
-        return copy[0][0]
-    }
+//     if(array.length === 1) {
+//         return array[0];
+//     }
+    
+//   const unique = [...new Set(array)];
+//     if(unique.length === 1) {
+//         return unique[0];
+//     }
+
+//   const newArray = unique.map((ite) => {
+//     let count = 0;
+
+//     for (i = 0; i < array.length; i++) {
+//       if (item === array[i]) {
+//         count++;
+//       }
+//     }
+
+//     return [item, count];
+//   });
+
+//   const copy = [...newArray];
+
+//   copy.sort((first, second) => {
+
+//     return second[1] - first[1];
+//   });
+    
+//     if(copy[0][1] === copy[1][1]) {
+//         return -1;
+//     }
+//     else {
+//         return copy[0][0]
+//     }
 }
