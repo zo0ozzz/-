8f86389335540efbfd742fe3e16bcf05707aea64
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-19 12:04:59 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,17 @@
 function solution(my_string) {
-    return my_string.replace(/[A-Z]/g, item => item.toLowerCase()).split('').sort().join('');
+    return [...my_string.toLowerCase()].sort().join('');
+    
+//     return [...my_string].sort((a, b) => {
+//         if(a.toLowerCase() < b.toLowerCase()) {
+//             return -1;
+//         }
+//         else if(a.toLowerCase() === b.toLowerCase()) {
+//             return 0;
+//         }
+//         else {
+//             return 1;
+//         }
+//     }).reduce((acc, cur) => acc + (cur.toLowerCase() === cur ? cur : cur.toLowerCase()), '');
+    
+//     return my_string.replace(/[A-Z]/g, item => item.toLowerCase()).split('').sort().join('');
 }
