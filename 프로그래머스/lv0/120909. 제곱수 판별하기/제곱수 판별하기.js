48b83bc6034e9827008efd58ae0af9b678ce8404
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-19 11:31:32 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,5 +1,9 @@
-function solution(n) {
+function solution(n) { 
     const 제곱근 = Math.sqrt(n);
     
-    return Math.floor(제곱근) === 제곱근 ? 1 : 2;    
+    return Number.isInteger(제곱근) ? 1 : 2;    
+
+//     const 제곱근 = Math.sqrt(n);
+    
+//     return Math.floor(제곱근) === 제곱근 ? 1 : 2;    
  }
