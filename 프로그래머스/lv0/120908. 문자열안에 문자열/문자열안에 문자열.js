03927667288da2655d8d36f424fe360a4768642e
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-19 11:07:30 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,5 +1,7 @@
-function solution(str1, str2) {
-    const reg = new RegExp(str2);
+function solution(str1, str2) {    
+    return str1.includes(str2) ? 1 : 2;
     
-    return reg.test(str1) ? 1 : 2;
+//     const reg = new RegExp(str2);
+    
+//     return reg.test(str1) ? 1 : 2;
 }
