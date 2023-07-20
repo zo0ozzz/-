3e9f56ec932edd2f80e32e0c9d54985a098e5bba
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-20 12:15:09 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,13 @@
+function solution(array, n) {
+    return array.filter(item => item === n).length;
+    
+//     let count = 0;
+    
+//     for(item of array) {
+//         if(item === n) {
+//             count++;
+//         }
+//     }
+    
+//     return count;
+}
