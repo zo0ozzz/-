bae3fd0788e16cb4ca763880e51669b7a0802ccc
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-20 10:53:06 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,15 @@
 function solution(array) {
+//     const str = array.join('');
+    
+//     let count = 0;
+    
+//     for(i = 0; i < str.length; i++) {
+//         if('7' === str[i]) {
+//             count++
+//         }    
+//     }
+    
+//     return count;
+    
     return array.join('').match(/7/g).length;
 }
