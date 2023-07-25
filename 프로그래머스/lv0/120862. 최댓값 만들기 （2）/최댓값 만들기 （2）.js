a661aa7dcd5e951e0e0c1592d54971a37e8d2308
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-26 01:26:02 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,13 +1,23 @@
 function solution(numbers) {
     let result = [];
     
-    for(item1 of numbers) {
-        for(item2 of numbers) {
-            if(numbers.indexOf(item1) !== numbers.lastIndexOf(item2)) {
-                result.push(item1 * item2);            
-            }
+    for(i = 0; i < numbers.length; i++) {
+        for(j = i + 1; j < numbers.length; j++) {
+            result.push(numbers[i] * numbers[j]);            
         }
     }
     
-   return Math.max(...result); 
+   return Math.max(...result);
+    
+//     let result = [];
+    
+//     for(item1 of numbers) {
+//         for(item2 of numbers) {
+//             if(numbers.indexOf(item1) !== numbers.lastIndexOf(item2)) {
+//                 result.push(item1 * item2);            
+//             }
+//         }
+//     }
+    
+//    return Math.max(...result); 
 }
