99c1b858316d74d154c0395c7e06f92b1128715a
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-25 23:37:42 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,22 +1,29 @@
 function solution(dots) {
-    let result = 0;
+    let x = [];
+    let y = [];
     
     for(item of dots) {
-        if(dots[0][0] !== item[0] && dots[0][1] !== item[1]) {
-            result = Math.abs(dots[0][0] - item[0]) * Math.abs(dots[0][1] - item[1])
-            break;
-        }
+        x.push(item[0]);
+        y.push(item[1]);
     }
     
-    return result;
+    return Math.abs(Math.max(...x) - Math.min(...x)) * Math.abs(Math.max(...y) - Math.min(...y))
     
-//    return dots.reduce((acc, cur) => {
-//        let result = 0;
-       
-//     if(acc[0] !== cur[0] && acc[1] !== cur[1]) {
-//         result = Math.abs(acc[0] - cur[0]) * Math.abs(acc[1] - cur[1])
-//     } 
-       
-//     return result;  
-//    }, dots[0])
+//     let result = 0;
+    
+//     for(item of dots) {
+//         if(dots[0][0] !== item[0] && dots[0][1] !== item[1]) {
+//             result = Math.abs(dots[0][0] - item[0]) * Math.abs(dots[0][1] - item[1])
+//             break;
+//         }
+//     }
+    
+//     return result;
+    
+   // return dots.reduce((acc, cur) => {       
+   //      if(dots[0][0] !== cur[0] && dots[0][1] !== cur[1]) {
+   //          acc = Math.abs(dots[0][0] - cur[0]) * Math.abs(dots[0][1] - cur[1])
+   //      } 
+   //  return acc
+   // }, 0)
 }
