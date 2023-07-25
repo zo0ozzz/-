23818b2a9acfab8087b8da38a46e39d50fe87ed7
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-26 02:09:44 +0900

    [level 0] Title: 다항식 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,46 @@
+function solution(polynomial) {
+    let x = 0;
+    let y = 0;
+    
+    let arr1 = [];
+    let arr2 = [];
+    
+    for(item of polynomial.split(' + ')) {
+        if(/x/.test(item)) {
+            if(item === 'x') {
+                x += 1;
+            }
+            else {
+                const plus = item.replace('x', '')
+                
+                x += +plus
+            }
+        }
+        else {
+            y += +item
+        }
+    }
+    
+    if(x !== 0 && y !== 0) {
+        if(x === 1) {
+            return `x + ${y}`;
+        }
+        else {
+            return `${x}x + ${y}`;    
+        }
+    }
+    
+    if(x === 0) {
+        return `${y}`
+    }
+    
+    if(y === 0) {
+        if(x === 1) {
+            return 'x';
+        }
+        else {
+            return `${x}x`        
+        }
+    }
+    
+}
