bd42b2437d9c20020ddd1fca7797aee3ab610955
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-07-26 01:04:47 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,42 +2,29 @@ function solution(keyinput, board) {
     const limitX = Math.floor(board[0] / 2);
     const limitY = Math.floor(board[1] / 2);
     
-    let arr = [0, 0]
+    let x = 0;
+    let y = 0;
     
     for(item of keyinput) {
         switch (item) {
             case 'up':
-                if(arr[1] === limitY) {
-                    break;
-                }
+                y !== limitY ? y += 1 : null;
                 
-                arr[1] += 1;    
                 break;
             case 'right':
-                if(arr[0] === limitX) {
-                    break;
-                }
+                x !== limitX ? x += 1 : null;
                 
-                arr[0] += 1;
                 break;
             case 'down':
-                if(arr[1] === 0 - limitY) {
-                    break;
-                }
+                y !== 0 - limitY ? y -= 1 : null;
                 
-                arr[1] -= 1;
                 break;
             case 'left':
-                if(arr[0] === 0 - limitX) {
-                    break;
-                }
+                x !== 0 - limitX ? x -= 1 : null;
                 
-                arr[0] -= 1;
                 break;
         }
-        
-        console.log(arr);
     }
     
-    return arr;
+    return [x, y];
 }
