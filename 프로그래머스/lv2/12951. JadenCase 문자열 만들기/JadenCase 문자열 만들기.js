143eb13fafacb3ff77764c65983fed6a6c542be6
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-08-08 17:20:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,17 +1,19 @@
 function solution(s) {
-    const str = s.toLowerCase().split(' ').reduce((acc, cur) => {
-        if(cur === '') {
-            cur = ' ';
-        }
-        else {
-            const reg = RegExp(cur[0]);
-            cur = cur.replace(reg, cur[0].toUpperCase());
-        }
+//     const str = s.toLowerCase().split(' ').reduce((acc, cur) => {
+//         if(cur === '') {
+//             cur = ' ';
+//         }
+//         else {
+//             const reg = RegExp(cur[0]);
+//             cur = cur.replace(reg, cur[0].toUpperCase());
+//         }
     
-        acc = acc + (cur !== ' ' ? (' ' + cur) : cur); 
+//         acc = acc + (cur !== ' ' ? (' ' + cur) : cur); 
         
-        return acc;
-    }, '')
+//         return acc;
+//     }, '')
     
-    return str.replace(' ', '');
+//     return str.replace(' ', '');
+    
+    return s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
 }
