0f7b53a32c7daf7ffe17b65b9edcf55884ae5e4c
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-08-22 23:45:46 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,22 +1,30 @@
 function solution(brown, yellow) {
-    let arr = [];
+    let result;
     
     for(i = yellow; i >= Math.sqrt(yellow); i--) {
         if(yellow % i === 0) {
-            arr.push([i, yellow / i]);
-        }
-    }
-    
-    let result;
-    
-    for(item of arr) {
-        if((item[0] + 2) * (item[1] + 2) - item[0] * item[1] === brown) {
-            result = item;
+            // arr.push([i, yellow / i]);
+            const 가로 = i;
+            const 세로 = yellow / i;
             
-            break;
+            if(2 * (가로 + 세로 + 2) === brown) {
+                result = [가로 + 2, 세로 + 2];
+                break;
+            }
         }
     }
     
-    return [result[0] + 2, result[1] + 2]
+//     let result;
     
+//     for(item of arr) {
+//         if(2 * (item[0] + item[1] + 2) === brown) {
+//             result = item;
+            
+//             break;
+//         }
+//     }
+    
+    // return [result[0] + 2, result[1] + 2]
+    
+    return result;
 }
