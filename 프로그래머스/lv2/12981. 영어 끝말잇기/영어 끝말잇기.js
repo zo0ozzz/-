8c5e8c78380e2368ebb8a94f283986cc1b2f3941
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-08-24 12:02:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,8 +1,8 @@
 function solution(n, words) {    
-    // 1
+    // 1 끝말잇기
     let index1;
 
-    for(i = 0; i < words.length - 1; i++) {
+    for(let i = 0; i < words.length - 1; i++) {
         const left = words[i];
         const right = words[i + 1];
         
@@ -13,8 +13,9 @@ function solution(n, words) {
         }
     }
     
-    // 2
-    let index2;
+    // 2 중복 단어 출현
+    
+    let arr = [];
     
 //     first: for (let i = 0; i < words.length - 1; i++) {
 //       for (let j = i + 1; j < words.length; j++) {
@@ -29,11 +30,14 @@ function solution(n, words) {
     for (let i = 0; i < words.length - 1; i++) {
       for (let j = i + 1; j < words.length; j++) {
         if (words[i] === words[j]) {
-          index2 = j;
+          arr.push(j);
+            
           break;
         }
       }
     }
+    
+    const index2 = arr.length === 0 ? undefined : Math.min(...arr);
 
     if(!index1 && !index2) {
         return [0, 0]
@@ -62,52 +66,3 @@ function whoWhen(n, index) {
     
     return [who, when];    
 }
-
-// function solution(n, words) {
-//   let index1;
-
-//   for (let i = 0; i < words.length - 1; i++) {
-//     const left = words[i];
-//     const right = words[i + 1];
-
-//     if (left[left.length - 1] !== right[0]) {
-//       index1 = i + 1;
-//       break;
-//     }
-//   }
-
-//   let index2;
-
-//   for (let i = 0; i < words.length - 1; i++) {
-//     for (let j = i + 1; j < words.length; j++) {
-//       if (words[i] === words[j]) {
-//         index2 = j;
-//         break;
-//       }
-//     }
-//   }
-
-//   if (!index1 && !index2) {
-//     return [0, 0];
-//   }
-
-//   if (!index1 && index2) {
-//     return whoWhen(n, index2);
-//   }
-
-//   if (index1 && !index2) {
-//     return whoWhen(n, index1);
-//   }
-
-//   if (index1 && index2) {
-//     const result = [index1, index2].sort((a, b) => a - b)[0];
-//     return whoWhen(n, result);
-//   }
-// }
-
-// function whoWhen(n, index) {
-//   const num = index + 1;
-//   const who = (num % n) || n;
-//   const when = Math.ceil(num / n);
-//   return [who, when];
-// }
