7fe8f8f91353c00010f4a8de5222d5b657805339
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-08-24 10:42:32 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,48 +1,113 @@
-function solution(n, words) {
-  let index1;
+function solution(n, words) {    
+    // 1
+    let index1;
 
-  for (let i = 0; i < words.length - 1; i++) {
-    const left = words[i];
-    const right = words[i + 1];
-
-    if (left[left.length - 1] !== right[0]) {
-      index1 = i + 1;
-      break;
+    for(i = 0; i < words.length - 1; i++) {
+        const left = words[i];
+        const right = words[i + 1];
+        
+        if(left[left.length - 1] !== right[0]) {
+            index1 = i + 1;
+                           
+            break;       
+        }
     }
-  }
+    
+    // 2
+    let index2;
+    
+//     first: for (let i = 0; i < words.length - 1; i++) {
+//       for (let j = i + 1; j < words.length; j++) {
+//         if (words[i] === words[j]) {
+//           index2 = j;
 
-  let index2;
+//           break first;
+//         }
+//       }
+//     }
 
-  for (let i = 0; i < words.length - 1; i++) {
-    for (let j = i + 1; j < words.length; j++) {
-      if (words[i] === words[j]) {
-        index2 = j;
-        break;
+    for (let i = 0; i < words.length - 1; i++) {
+      for (let j = i + 1; j < words.length; j++) {
+        if (words[i] === words[j]) {
+          index2 = j;
+          break;
+        }
       }
     }
-  }
 
-  if (!index1 && !index2) {
-    return [0, 0];
-  }
-
-  if (!index1 && index2) {
-    return whoWhen(n, index2);
-  }
-
-  if (index1 && !index2) {
-    return whoWhen(n, index1);
-  }
-
-  if (index1 && index2) {
-    const result = [index1, index2].sort((a, b) => a - b)[0];
-    return whoWhen(n, result);
-  }
+    if(!index1 && !index2) {
+        return [0, 0]
+    }
+    
+    if(!index1 && index2) {
+        return whoWhen(n, index2);
+    }
+    
+    if(index1 && !index2) {
+        return whoWhen(n, index1)
+    }
+    
+    if(index1 && index2) {
+        const result = [index1, index2].sort((a, b) => a - b)[0];
+        
+        return whoWhen(n, result)
+    }
 }
 
 function whoWhen(n, index) {
-  const num = index + 1;
-  const who = (num % n) || n;
-  const when = Math.ceil(num / n);
-  return [who, when];
+    const num = index + 1;
+    
+    const who = (num % n) || n;
+    const when = Math.ceil(num/ n)
+    
+    return [who, when];    
 }
+
+// function solution(n, words) {
+//   let index1;
+
+//   for (let i = 0; i < words.length - 1; i++) {
+//     const left = words[i];
+//     const right = words[i + 1];
+
+//     if (left[left.length - 1] !== right[0]) {
+//       index1 = i + 1;
+//       break;
+//     }
+//   }
+
+//   let index2;
+
+//   for (let i = 0; i < words.length - 1; i++) {
+//     for (let j = i + 1; j < words.length; j++) {
+//       if (words[i] === words[j]) {
+//         index2 = j;
+//         break;
+//       }
+//     }
+//   }
+
+//   if (!index1 && !index2) {
+//     return [0, 0];
+//   }
+
+//   if (!index1 && index2) {
+//     return whoWhen(n, index2);
+//   }
+
+//   if (index1 && !index2) {
+//     return whoWhen(n, index1);
+//   }
+
+//   if (index1 && index2) {
+//     const result = [index1, index2].sort((a, b) => a - b)[0];
+//     return whoWhen(n, result);
+//   }
+// }
+
+// function whoWhen(n, index) {
+//   const num = index + 1;
+//   const who = (num % n) || n;
+//   const when = Math.ceil(num / n);
+//   return [who, when];
+// }
