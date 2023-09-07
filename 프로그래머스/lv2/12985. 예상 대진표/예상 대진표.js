4c7331f3327caac8899ce54c5f902ba231817d85
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-09-07 17:48:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,12 +1,17 @@
 function solution(n,a,b) {
     let count = 1;
     
-    while(a !== b) {
+    while(true) {
         a = a % 2 === 0 ? a / 2 : (a + 1) / 2;
         b = b % 2 === 0 ? b / 2 : (b + 1) / 2;
         
-        count++
+        if(a === b) {
+            break;
+        }
+        else {
+            count++
+        }
     }
     
-    return count - 1;
+    return count;
 }
