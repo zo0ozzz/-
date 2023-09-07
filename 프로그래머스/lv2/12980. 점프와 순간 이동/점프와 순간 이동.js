1c0b8735614429519ea0d338dbf2eb886522d43f
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-09-07 17:23:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.10 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,26 +1,20 @@
 function solution(n){
-//     let result = 0;
+    // 최대한 순간이동을 많이 하게.
+    // n이 2의 배수라면 무조건 순간이동이 유리함.
+    // n이 2의 배수가 아니라면 (n - 1) / 2까지는 점프로 도달 후 순간이동.
     
-//     for(i = 1; i++;) {
-//         if(Math.pow(2, i) <= n && n < Math.pow(2, i + 1)) {
-//             result = i;
-            
-//             break;
-//         }
-//     }
-    
-//     return n - Math.pow(2, result) + 1
     let jumpCount = 0;
     
-    while(n > 1) {
+    while(n !== 1) {
         if(n % 2 === 0) {
             n = n / 2;
         }
-        else {
+        else if(n % 2 !== 0) {
             jumpCount++
             n = (n - 1) / 2;
         }
         
     }
+    
     return jumpCount + 1
 }
